f8ced3b23712cda518ed777460c189630fc113c8
Amit <sriv.amit@gmail.com>
2023-09-05 16:22:41 +0530

    Update replaceVar.sh

--- replaceVar.sh
+++ replaceVar.sh
@@ -24,63 +24,63 @@ pwd
 
 
 #APP_NAME
-sed -i "s/#KS_APP_NAME#/app/g" /workspace/output/kube-deploy.yaml
+sed -i "s/#KS_APP_NAME#/app/g" /workspace/source/kube-deploy.yaml
 
 #KS_INGRESS_LARGE_CLIENT_HEADER_BUFFERS
-sed -i "s/#KS_INGRESS_LARGE_CLIENT_HEADER_BUFFERS#/4 16k/g" /workspace/output/kube-deploy.yaml
+sed -i "s/#KS_INGRESS_LARGE_CLIENT_HEADER_BUFFERS#/4 16k/g" /workspace/source/kube-deploy.yaml
 
 #KS_HEALTH_SCHEME
-sed -i "s/#KS_HEALTH_SCHEME#/HTTP/g" /workspace/output/kube-deploy.yaml
+sed -i "s/#KS_HEALTH_SCHEME#/HTTP/g" /workspace/source/kube-deploy.yaml
 
 #KS_CLUSTER_SUBDOMAIN
-sed -i "s/#KS_CLUSTER_SUBDOMAIN#/saashub-next-gen-test-7543ffbb0026f72e4c4bdf341a6ee84a-0000/g" /workspace/output/kube-deploy.yaml
+sed -i "s/#KS_CLUSTER_SUBDOMAIN#/saashub-next-gen-test-7543ffbb0026f72e4c4bdf341a6ee84a-0000/g" /workspace/source/kube-deploy.yaml
 
 #KS_REGION
-sed -i "s/#KS_REGION#/us-south/g" /workspace/output/kube-deploy.yaml
+sed -i "s/#KS_REGION#/us-south/g" /workspace/source/kube-deploy.yaml
 
 #KS_APP_PORT
-sed -i "s/#KS_APP_PORT#/3737/g" /workspace/output/kube-deploy.yaml
+sed -i "s/#KS_APP_PORT#/3737/g" /workspace/source/kube-deploy.yaml
 
 #KS_INSTANCES
-sed -i "s/#KS_INSTANCES#/2/g" /workspace/output/kube-deploy.yaml
+sed -i "s/#KS_INSTANCES#/2/g" /workspace/source/kube-deploy.yaml
 
 #KS_SECRET_NAME
-sed -i "s/#KS_SECRET_NAME#/saashub-next-gen-test-7543ffbb0026f72e4c4bdf341a6ee84a-0000/g" /workspace/output/kube-deploy.yaml
+sed -i "s/#KS_SECRET_NAME#/saashub-next-gen-test-7543ffbb0026f72e4c4bdf341a6ee84a-0000/g" /workspace/source/kube-deploy.yaml
 
 #KS_MEMORY
-sed -i "s/#KS_MEMORY#/128Mi/g" /workspace/output/kube-deploy.yaml
+sed -i "s/#KS_MEMORY#/128Mi/g" /workspace/source/kube-deploy.yaml
 
 #KS_CPU
-sed -i "s/#KS_CPU#/0.01/g" /workspace/output/kube-deploy.yaml
+sed -i "s/#KS_CPU#/0.01/g" /workspace/source/kube-deploy.yaml
 
 #KS_HEALTH_ROUTE
-sed -i "s/#KS_HEALTH_ROUTE#/\//g" /workspace/output/kube-deploy.yaml
+sed -i "s/#KS_HEALTH_ROUTE#/\//g" /workspace/source/kube-deploy.yaml
 
 #KS_HEALTH_DELAY
-sed -i "s/#KS_HEALTH_DELAY#/20/g" /workspace/output/kube-deploy.yaml
+sed -i "s/#KS_HEALTH_DELAY#/20/g" /workspace/source/kube-deploy.yaml
 
 #KS_HEALTH_TIMEOUT
-sed -i "s/#KS_HEALTH_TIMEOUT#/2/g" /workspace/output/kube-deploy.yaml
+sed -i "s/#KS_HEALTH_TIMEOUT#/2/g" /workspace/source/kube-deploy.yaml
 
 #KS_HEALTH_INTERVAL
-sed -i "s/#KS_HEALTH_INTERVAL#/11/g" /workspace/output/kube-deploy.yaml
+sed -i "s/#KS_HEALTH_INTERVAL#/11/g" /workspace/source/kube-deploy.yaml
 
 #KS_LIVENESS_ROUTE
-sed -i "s/#KS_LIVENESS_ROUTE#/\//g" /workspace/output/kube-deploy.yaml
+sed -i "s/#KS_LIVENESS_ROUTE#/\//g" /workspace/source/kube-deploy.yaml
 
 #KS_LIVENESS_TIMEOUT
-sed -i "s/#KS_LIVENESS_TIMEOUT#/15/g" /workspace/output/kube-deploy.yaml
+sed -i "s/#KS_LIVENESS_TIMEOUT#/15/g" /workspace/source/kube-deploy.yaml
 
 #KS_LIVENESS_INTERVAL
-sed -i "s/#KS_LIVENESS_INTERVAL#/310/g" /workspace/output/kube-deploy.yaml
+sed -i "s/#KS_LIVENESS_INTERVAL#/310/g" /workspace/source/kube-deploy.yaml
 
 #KS_LIVENESS_THRESHOLD
-sed -i "s/#KS_LIVENESS_THRESHOLD#/3/g" /workspace/output/kube-deploy.yaml
+sed -i "s/#KS_LIVENESS_THRESHOLD#/3/g" /workspace/source/kube-deploy.yaml
 
 #KS_MEMORY_LIMIT
-sed -i "s/#KS_MEMORY_LIMIT#/$KS_MEMORY_LIMIT/g" /workspace/output/kube-deploy.yaml
+sed -i "s/#KS_MEMORY_LIMIT#/$KS_MEMORY_LIMIT/g" /workspace/source/kube-deploy.yaml
 
 #KS_CPU_LIMIT
-sed -i "s/#KS_CPU_LIMIT#/$KS_CPU_LIMIT/g" /workspace/output/kube-deploy.yaml
+sed -i "s/#KS_CPU_LIMIT#/$KS_CPU_LIMIT/g" /workspace/source/kube-deploy.yaml
 
-cat /workspace/output/kube-deploy.yaml
+cat /workspace/source/kube-deploy.yaml
